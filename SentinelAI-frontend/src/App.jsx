aa1f510399747a187d73e3a297ead5033793dd0a
aravindaravindreddy812-cyber <aravindaravindreddy812@gmail.com>
2026-09-21 18:39:25 +0530

    Change API endpoint to production URL

--- SentinelAI-frontend/src/App.jsx
+++ SentinelAI-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
 
-const API = 'http://127.0.0.1:8000'
+const API = 'https://sentinel-ai-uoug.onrender.com'
 const VIDEO_URL = `${API}/video`
 
 const demoIncident = {
